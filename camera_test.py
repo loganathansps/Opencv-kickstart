0fb6a4dd6e586c4b6797fa586b15ee2113ff1ac1
Loga <loganathans.p.s@gmail.com>
2020-05-04 16:45:38 +0530

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -1,11 +1,16 @@
-import cv2
+######################################################
+###Program to validate opencv and camera integrity ###
+######################################################
 
-cap = cv2.VideoCapture(0)
+import cv2 #opencv library 
 
-while(True):
-    ret,frame = cap.read()
-    cv2.imshow('video',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+cap = cv2.VideoCapture(0) #Initiating camera. '0' refers to inbuilt/default camera.
+                          #change number to switch camera
+
+while(True): #indefinite loop to handle frames one by one. 
+    ret,frame = cap.read() #Read the camera frame
+    cv2.imshow('video',frame) #Display/show the frame
+    if cv2.waitKey(1) & 0xFF == ord('q'): # press 'q' to terminate the program.
         break
 
-cap.release()
+cap.release() #Terminate/release the camera
